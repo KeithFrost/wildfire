6c52bb29a1747168d264fba6b7b6e9726ae505cb
Keith Frost <keith.l.frost@gmail.com>
2013-10-24 16:00:24 -0400

    minimal bittorrent client

--- wildfire-client/src/main/scala/org/wildfire/client/client.scala
+++ wildfire-client/src/main/scala/org/wildfire/client/client.scala
@@ -1,10 +1,18 @@
 package org.wildfire.client
 
+import java.io.File
+import java.net.InetAddress
+
+import com.turn.ttorrent.client.{Client, SharedTorrent}
+
 object Main {
   def main(args: Array[String]) {
-    println("does nothing")
+    val client = new Client(
+      InetAddress.getLocalHost,
+      SharedTorrent.fromFile(new File(args(0)), new File(args(1))))
+    client.download()
+    println("Download from torrent %s begun to %s".format(args(0), args(1)))
+    client.waitForCompletion()
+    println("Download Complete!")
   }
-}
-
-
-  
+  }
