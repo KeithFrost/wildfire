74f40aa3ea8faaecdce772859a96fd074fb26599
Keith Frost <keith.l.frost@gmail.com>
2013-10-24 21:57:42 -0400

    allow seeding

--- wildfire-client/src/main/scala/org/wildfire/client/client.scala
+++ wildfire-client/src/main/scala/org/wildfire/client/client.scala
@@ -19,19 +19,9 @@ object Main {
     val localHost = InetAddress.getLocalHost
     val client = new Client(
       localHost, SharedTorrent.fromFile(new File(args(0)), new File(args(1))))
-    val start = System.currentTimeMillis
-    client.download()
-    println("Download from torrent %s begun to %s".format(args(0), args(1)))
-    client.waitForCompletion()
-    val elapsed = System.currentTimeMillis - start
-    println("Download Completed after %f seconds".format(elapsed * 0.001))
-    while (true) {
-      Thread.sleep(1000)
-    }
+    client.share(-1)
   }
 }
-
-//  "dns://10.183.138.92"
 
 class Dns(source: String) {
   val env = new Properties()
